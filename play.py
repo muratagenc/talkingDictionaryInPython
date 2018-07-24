ec91d1698b552a30838bc7c3ef44c6d7fd46cb99
Murat A. Genc <muratagenc@gmail.com>
2018-07-23 21:27:36 -0400

    Add files via upload

--- play.py
+++ play.py
@@ -3,21 +3,24 @@ import sys
 import urllib.request, re
 from playsound import playsound
 
-print("This program requires internet connection")
+print("This program requires internet connection. Type your word and hit enter")
+print("by Murat Genc. mgenc@pace.edu")
 
 while(True):
 	#read the word
-	wordText = input("Word: ")
+	wordText = input(">")
 	if(wordText == "q"):
 		break
 	try:	
 		#to get description of the word
 		url="http://"+ "www" + "." + "merriam-webster" + ".com/dictionary/" #link
+		#url = "http://www.dictionary.com/cgi-bin/dict.pl?term=%s" % wordText
 		url=url+wordText
 		text=urllib.request.urlopen(url).read().decode('utf-8') #open the url, read it and change the encoding to utf-8. Needed to use regex on it
-		text = re.findall('<span><span class="intro-colon">:</span>(.+?)</span>', text) 
+		print(re.findall('<meta name="description" content="(.+?)">', text))
+		print(re.findall('<span><span class="intro-colon">:</span>(.+?)</span>', text))
+
 		#print(re.findall('<span><span class="intro-colon">:</span>(.+?)</span>', text)) #regex finds all occurences of the specific
-		print(text)
 	except:
 		pass
 	
