facf3dcd987810e753ed163d6de2555db84d0494
DeliKasif <muratagenc@gmail.com>
2018-07-25 23:31:14 -0400

    initial upload of the new version

--- play.py
+++ play.py
@@ -3,6 +3,48 @@ import sys
 import urllib.request, re
 from playsound import playsound
 
+def getSound(word):
+	#now get the sound of it
+	fileName = word + ".mp3"
+	try:
+		playsound("./audio/" + word[0] + "/" + fileName)
+	except:
+		print("audio in database")
+		wordAudioURL = "https://ssl.gstatic.com/dictionary/static/sounds/oxford/" + word + "--_gb_1.mp3"
+		#print(wordAudioURL)
+		try:
+			response = urllib.request.urlretrieve(wordAudioURL, "./data/" + fileName)
+			playsound("./audio/" + word[0] + "/" + fileName)
+		except:
+			print("audio not on the net")
+			pass
+
+
+
+def getDefinition(word):
+	try:
+		with open("./descriptions/" + word[0] + "/" + word + ".txt", 'r') as fin:
+			print(fin.read(), end="")
+
+	except:	
+		url="http://"+ "www" + "." + "merriam-webster" + ".com/dictionary/" #link
+		url=url+wordText
+		text=urllib.request.urlopen(url).read().decode('utf-8') 
+		str2 = re.findall('<span><span class="intro-colon">:</span>(.+?)</span>', text)
+		#print("from the net: ", str2)
+
+		if len(str2) > 0:
+			file = open("./descriptions/" + word[0] + "/" + word + ".txt", 'w+')
+			
+			str1 = ''.join(str2)
+			print(str1)
+			file.write(str1)
+			file.close()
+
+
+
+
+##main program
 print("This program requires internet connection. Type your word and hit enter")
 print("by Murat Genc. mgenc@pace.edu")
 
@@ -11,32 +53,11 @@ while(True):
 	wordText = input(">")
 	if(wordText == "q"):
 		break
-	try:	
-		#to get description of the word
-		url="http://"+ "www" + "." + "merriam-webster" + ".com/dictionary/" #link
-		#url = "http://www.dictionary.com/cgi-bin/dict.pl?term=%s" % wordText
-		url=url+wordText
-		text=urllib.request.urlopen(url).read().decode('utf-8') #open the url, read it and change the encoding to utf-8. Needed to use regex on it
-		print(re.findall('<meta name="description" content="(.+?)">', text))
-		print(re.findall('<span><span class="intro-colon">:</span>(.+?)</span>', text))
-
-		#print(re.findall('<span><span class="intro-colon">:</span>(.+?)</span>', text)) #regex finds all occurences of the specific
-	except:
-		pass
 	
+	getDefinition(wordText)	
+	getSound(wordText)
 
-	#now get the sound of it
-	fileName = wordText + ".mp3"
-	try:
-		playsound("./data/" + fileName)
-	except:
-		print("not in database")
-		wordAudioURL = "https://ssl.gstatic.com/dictionary/static/sounds/oxford/" + wordText + "--_gb_1.mp3"
-		#print(wordAudioURL)
-		try:
-			response = urllib.request.urlretrieve(wordAudioURL, "./data/" + fileName)
-			playsound("./data/" + fileName)
-		except:
-			print("not on the net")
-			pass
+print("bye!")
+
+
 
