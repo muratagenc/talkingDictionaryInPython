02cf3c7b5e9e6d32e9407194e6dfaaef8c283e06
DeliKasif <muratagenc@gmail.com>
2018-07-26 23:25:30 -0400

    all files edited to remove unwanted text

--- play.py
+++ play.py
@@ -2,6 +2,13 @@
 import sys
 import urllib.request, re
 from playsound import playsound
+import os
+from random import randint
+import time
+
+
+
+
 
 def getSound(word):
 	#now get the sound of it
@@ -9,7 +16,7 @@ def getSound(word):
 	try:
 		playsound("./audio/" + word[0] + "/" + fileName)
 	except:
-		print("audio in database")
+		print("audio not in the database")
 		wordAudioURL = "https://ssl.gstatic.com/dictionary/static/sounds/oxford/" + word + "--_gb_1.mp3"
 		#print(wordAudioURL)
 		try:
@@ -21,43 +28,115 @@ def getSound(word):
 
 
 
+
+
+
 def getDefinition(word):
 	try:
 		with open("./descriptions/" + word[0] + "/" + word + ".txt", 'r') as fin:
 			print(fin.read(), end="")
 
 	except:	
-		url="http://"+ "www" + "." + "merriam-webster" + ".com/dictionary/" #link
-		url=url+wordText
-		text=urllib.request.urlopen(url).read().decode('utf-8') 
-		str2 = re.findall('<span><span class="intro-colon">:</span>(.+?)</span>', text)
-		#print("from the net: ", str2)
+		print("description not in the database")
+		try:
+			url="http://"+ "www" + "." + "merriam-webster" + ".com/dictionary/" #link
+			url=url+wordText
+			text=urllib.request.urlopen(url).read().decode('utf-8') 
+			str2 = re.findall('<span><span class="intro-colon">:</span>(.+?)</span>', text)
+			#print("from the net: ", str2)
 
-		if len(str2) > 0:
-			file = open("./descriptions/" + word[0] + "/" + word + ".txt", 'w+')
+			if len(str2) > 0:
+				file = open("./descriptions/" + word[0] + "/" + word + ".txt", 'w+')
 			
-			str1 = ''.join(str2)
-			print(str1)
-			file.write(str1)
-			file.close()
+				str1 = ''.join(str2)
+				print(str1)
+				file.write(str1)
+				file.close()
+		except:
+			print("description not on the net")
+			pass		
 
 
 
 
-##main program
+
+
+
+def getFileList():
+	#fill the array with file names
+	fileList = []
+	for dirpath, dirs, files in os.walk("./descriptions/"):
+		fileList = fileList + files
+
+	size = len(fileList)
+
+	#print(fileList[120][:-4])
+	
+	filePicked = fileList[randint(0,size)][:-4]
+
+	try:
+		with open("./descriptions/" + filePicked[0] + "/" + filePicked + ".txt", 'r') as fin:
+			description = fin.read()
+	except:
+		description =''
+		pass
+	
+	#print(filePicked)
+	#print(description)
+	arr = [filePicked, description]
+
+	#return the word and its description text
+	return arr
+
+
+
+
+
+
+def wordGame():
+	while(True):
+	#print(getFileList())
+		os.system('clear')
+		item = getFileList()
+		theQuestion = item[0]
+		theAnswer = item[1]
+		print(theQuestion)
+		getSound(theQuestion)
+		time.sleep(1)
+		print(theAnswer)		
+
+		#press enter to continue
+		pause = input("")
+		if pause:
+			break
+
+
+
+
+
+#main program
 print("This program requires internet connection. Type your word and hit enter")
 print("by Murat Genc. mgenc@pace.edu")
 
-while(True):
-	#read the word
-	wordText = input(">")
-	if(wordText == "q"):
-		break
-	
-	getDefinition(wordText)	
-	getSound(wordText)
+choice = input("(p)lay the game or (t)train? [t]: ")
 
-print("bye!")
+if choice == "t" or choice == "":
+	while(True):
+		#read the word
+		wordText = input(">")
+		if(wordText == "q"):
+			break
+	
+		getDefinition(wordText)
+		print()
+		getSound(wordText)
+	
+	print("bye!")
+else:
+	print("let's play!")
+	wordGame()
+
+
 
 
 
